dacc9d4404ca453a5335a8df4347bc215e44645d
luixlacrux <luixlacrux18@gmail.com>
2017-06-03 21:03:05 -0400

    Add eslint config file

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
-// import NotFound from
+
 import GistList from '../containers/GistList'
 import GistDetail from '../containers/GistDetail'
 
